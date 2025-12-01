8227628cfaaf737cb2f0580c81dd0f7b32abc8a7
Bao <s3958122@rmit.edu.vn>
2025-12-01 07:31:13 +0700

    Update HistoryPage.jsx
    small update

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -1,10 +1,7 @@
 import "./HistoryPage.css";
 
 const mockHistory = [
-  { id: 1, title: "A lush, vibrant green tree...", date: "11/5/2024" },
-  { id: 2, title: "A girl with blue hair...", date: "6/5/2023" },
-  { id: 3, title: "A cat wearing a hat...", date: "25/7/2023" },
-  { id: 4, title: "An egg with the rock face...", date: "11/2/2025" },
+  { id: 1, title: "Picture1", date: "11/5/2024" },
 ];
 
 const HistoryPage = () => {
